00f5b9a60aba3268c8426feefdbdb84b1258006b
hanx_smile <hanx_smile@icloud.com>
2023-08-31 16:39:44 +0800

    debug

--- vigc/datasets/datasets/intern_datasets/coco_pseudo_dataset.py
+++ vigc/datasets/datasets/intern_datasets/coco_pseudo_dataset.py
@@ -1,4 +1,5 @@
 from vigc.datasets.datasets.base_dataset import BaseDataset
+import torch
 
 
 class COCO_Pseudo_Dataset(BaseDataset):
@@ -22,4 +23,20 @@ class COCO_Pseudo_Dataset(BaseDataset):
             "text_input": conv_text,
             'data_type': 'conversation',
             'det_res': "",
+        }
+
+    def collater(self, samples):
+        image_list, text_input_list, data_type_list, det_res_list = [], [], [], []
+
+        for sample in samples:
+            image_list.append(sample["image"])
+            text_input_list.append(sample["text_input"])
+            data_type_list.append(sample["data_type"])
+            det_res_list.append(sample["det_res"])
+
+        return {
+            "image": torch.stack(image_list, dim=0),
+            "text_input": text_input_list,
+            "data_type": data_type_list,
+            "det_res": det_res_list
         }
