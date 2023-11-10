f1e84f9ad73415c9831502a7833cfab7ff2b0130
hanx_smile <hanx_smile@icloud.com>
2023-11-10 20:21:25 +0800

    debug

--- vigc/datasets/datasets/dpo_exp_datasets/pope_test_dataset.py
+++ vigc/datasets/datasets/dpo_exp_datasets/pope_test_dataset.py
@@ -32,7 +32,7 @@ class POPETestDataset(BaseDataset):
         ann = self.samples[index]
 
         image = self.vis_processor(self._read_image(ann))
-        question = self.text_processor(ann["question"])
+        question = self.text_processor(ann["question"]).replace("\n", "")
 
         prompt = random.choice(self.PROMPTS)
         question = prompt.format(q=question)
